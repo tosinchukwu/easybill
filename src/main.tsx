d7a2dd145f879550f32f0db2c149858ec06bcc58
Abdulqawiyyi abiodun <86069758+horlash-Dev@users.noreply.github.com>
2026-02-21 15:43:09 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,5 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
 
-import MiniApp from "@base-org/sdk";
-
-const root = createRoot(document.getElementById("root")!);
-
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// REQUIRED FOR BASE MINI APP
-MiniApp.ready();
+createRoot(document.getElementById("root")!).render(<App />);
