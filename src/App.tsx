cbf8ff8d6c19a6d3d8003b10b6d4c4d3881fe1eb
Abdulqawiyyi abiodun <86069758+horlash-Dev@users.noreply.github.com>
2026-02-21 08:37:39 +0100

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,8 @@
-import { Toaster } from "@/components/ui/toaster";
-import { Toaster as Sonner } from "@/components/ui/sonner";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { BrowserRouter, Routes, Route } from "react-router-dom";
-import { HashRouter, Routes, Route } from "react-router-dom";
-import Index from "./pages/Index";
-import NotFound from "./pages/NotFound";
-
-const queryClient = new QueryClient();
-
-const App = () => (
-  <QueryClientProvider client={queryClient}>
-    <TooltipProvider>
-      <Toaster />
-      <Sonner />
-      <HashRouter>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </HashRouter>
-    </TooltipProvider>
-  </QueryClientProvider>
-);
-
-export default App;
+export default function App() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>EazyBill loaded</h1>
+      <p>If you see this, React mounted successfully.</p>
+    </div>
+  );
+}
