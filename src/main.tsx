f4026b27d1eb9c076048a330d48d5da3cb6f47b0
Abdulqawiyyi abiodun <86069758+horlash-Dev@users.noreply.github.com>
2026-02-21 09:14:55 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,28 +1,5 @@
-if (typeof window !== "undefined" && isBaseApp()) {
-  // Prevent eager wallet listeners from initializing
-  // but do NOT fully delete ethereum
-  try {
-    Object.defineProperty(window, "ethereum", {
-      configurable: true,
-      get() {
-        return undefined;
-      }
-    });
-  } catch {}
-}
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
 
-const rootEl = document.getElementById("root");
-
-if (rootEl) {
-  requestAnimationFrame(() => {
-    createRoot(rootEl).render(<App />);
-  });
-}
-// import { createRoot } from "react-dom/client";
-// import App from "./App.tsx";
-// import "./index.css";
-
-// createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(<App />);
