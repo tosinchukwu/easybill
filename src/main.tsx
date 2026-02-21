f9c11eae5a85beca8b2d1aadb3b09b2eaecebd18
Abdulqawiyyi abiodun <86069758+horlash-Dev@users.noreply.github.com>
2026-02-21 09:06:35 +0100

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,10 @@
+// Block injected wallets in Base Mini App
+if (typeof window !== "undefined") {
+  // @ts-ignore
+  window.ethereum = undefined;
+  // @ts-ignore
+  window.web3 = undefined;
+}
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
